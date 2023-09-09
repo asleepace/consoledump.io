bbf8931b301979671727bbbf74bb11cdb47a0db6
Colin <colin_teahan@yahoo.com>
2023-09-09 01:57:52 -0700

    adding styles

--- src/client/index.js
+++
@@ -1,5 +0,0 @@
-
-
-console.dump({
-  url: 'https://consoledump.io/b44n23cs783',
-})
---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import { Elysia, t, ws } from 'elysia'
+import { html } from '@elysiajs/html'
+import { staticPlugin } from '@elysiajs/static'
+
+const app = new Elysia()
+  .use(html())
+  .use(staticPlugin())
+  .get('/', ({ html }) => {
+
+    return Bun.file("./src/html/index.html").text()
+  })
+  .get('/editor', ({ html }) => {
+    return Bun.file("./src/html/editor.html").text()
+  })
+  .listen(3000)
+
+console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
+
