72babfbb5c7ae660a6f800490468c9b54ab31673
Colin <colin_teahan@yahoo.com>
2023-09-11 00:32:31 -0700

    server-side-session creation looking good

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { staticPlugin } from '@elysiajs/static'
 import { ElysiaWS, ElysiaWSContext, WSTypedSchema } from 'elysia/dist/ws'
 import { WebSocketConnections } from './api/server-sockets'
 import { cors } from '@elysiajs/cors'
-import { generateRandomSession } from './api/generate-session';
 
 // status
 const Status = {
@@ -41,6 +40,15 @@ const app = new Elysia()
   .onRequest((context) => {
     console.log(context)
   })
+  // static routes go here so that they won't be overriden by the wildcard, same
+  // goes for the websocket routes.
+  .get('/create', ({ set, ip }) => {
+    const sessionId = connections.createSession()
+    connections.dump(ip, JSON.stringify(`created session ${sessionId}`))
+    set.redirect = `/${sessionId}`
+    set.status = 302
+    return "redirecting..."
+  })
   // handle websocket connection from the client, we want to do some
   // bookkeeping here to keep track of all active connections. When opened
   // we will save the ws to connections with the corresponding path and
@@ -67,12 +75,6 @@ const app = new Elysia()
   })
   .get('/', conext => {
     return Bun.file("./src/html/homepage.html").text()
-  })
-  .get('/create', ({ set, ip }) => {
-    const sessionId = connections.createSession()
-    connections.broadcast(ip, [`created session ${sessionId}`])
-    set.redirect = `/${sessionId}`
-    set.status = 302
   })
   .post('/', ({ body, ip }) => {
     console.log('[server] received message:', body)
