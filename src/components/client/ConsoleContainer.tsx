d12864afa968c66fbb211c8cb66800b2388eb4cf
Colin <colin_teahan@yahoo.com>
2025-03-22 12:43:21 -0700

    basic pipe stream now working

--- src/components/client/ConsoleContainer.tsx
+++ src/components/client/ConsoleContainer.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useConsoleDump } from './useConsoleDump'
 
 export type ConsoleMessage = {
   createdAt: Date
@@ -14,20 +15,20 @@ export function ConsoleContainer({
   sessionId,
   initialMessages = [],
 }: ConsoleContainerProps) {
-  const [messages, setMessages] = useState<ConsoleMessage[]>(initialMessages)
+  const { messages } = useConsoleDump(sessionId)
 
   return (
-    <div className="w-full flex flex-col flex-1 border-1 basis-1/2 border-white/10 overflow-x-hidden bg-slate-950 overflow-y-auto">
-      {messages.map((msg) => {
+    <div className="w-full flex flex-col flex-1 border-1 basis-1/2 border-white/10 pt-2 overflow-x-hidden bg-slate-950 overflow-y-auto">
+      {messages.map((msg, index) => {
         return (
           <div
-            className="flex flex-row self-stretch p-2 px-4 bottom-b-1 bottom-b-white/10 items-center gap-x-2 text-white/90 font-mono text-sm"
-            key={msg.message}
+            className="flex flex-row self-stretch p-0.5 px-4 bottom-b-1 bottom-b-white/10 items-center gap-x-4 text-white/90 font-mono text-sm"
+            key={String(`message-${index}`)}
           >
-            <p className="text-white/40">
+            <p className="text-white/40 text-sm">
               {msg.createdAt.toLocaleString('en-US', {
                 timeStyle: 'medium',
-              }) + ': '}
+              })}
             </p>
             <p>{msg.message}</p>
           </div>
