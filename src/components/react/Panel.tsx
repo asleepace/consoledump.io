b387a7d4fadf2aa39ac5602345490092805dc343
Colin <colin_teahan@yahoo.com>
2025-10-12 15:08:46 -0700

    fixed some z-index issues with panels

--- src/components/react/Panel.tsx
+++ src/components/react/Panel.tsx
@@ -64,7 +64,7 @@ export function Panel({
       {!isOpen && (
         <button
           onClick={handleOpen}
-          className="fixed right-0 top-1/2 -translate-y-1/2 bg-zinc-800 text-gray-300 p-2 rounded-l-md hover:bg-zinc-700 transition-colors z-40"
+          className="fixed right-0 top-1/2 -translate-y-1/2 bg-zinc-800 text-gray-300 p-2 rounded-l-md hover:bg-zinc-700 transition-colors z-60"
         >
           <ChevronLeft size={20} />
         </button>
@@ -81,7 +81,7 @@ export function Panel({
       {/* --- Side panel --- */}
       <div
         className={cn(
-          `fixed right-0 font-mono top-0 h-full max-h-screen w-full md:w-128 bg-zinc-900 text-gray-200 z-50 transform transition-transform duration-300 ease-in-out`,
+          `fixed right-0 font-mono top-0 h-full max-h-screen w-full md:w-128 bg-zinc-900 text-gray-200 z-60 transform transition-transform duration-300 ease-in-out`,
           isOpen ? 'translate-x-0' : 'translate-x-full',
           className
         )}
